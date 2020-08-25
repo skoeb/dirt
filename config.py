6df9beb7b58a46493218150f2be83d2eb794989b
skoeb <samkoebrich@gmail.com>
2020-08-24 23:15:02 -0600

    adding procfile

--- config.py
+++ config.py
@@ -3,15 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI  = os.environ.get('DB_CONN_STR')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_CONN_STR')
     SCHEMA = 'production'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    # ADMINS = ['your-email@example.com']
-    # LANGUAGES = ['en', 'es']
-    # MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-    # POSTS_PER_PAGE = 25
+    LOG_TO_STDOUT = True
